0cc93109b77dc2e73824f4a85875dd9793ebedf1
olani siyum <olisiyum@gmail.com>
2024-11-17 00:21:17 +0300

    setting up for deployement

--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h4yp7wwgq#x5-=3!^^9ro^x!9+@rjl#wjm7eb2a_$qu3z@(0$9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app"]
 
